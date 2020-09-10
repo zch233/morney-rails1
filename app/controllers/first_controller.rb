0565e7af76edbc7b26d62c19bd242f4799725071
zhangchao <529743595@qq.com>
2020-09-10 20:41:54 +0800

    Suport Render HTML

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,5 +1,7 @@
 class FirstController < ApplicationController
   def hello
-    render plain: 'hello'
+    # render plain: 'hello rails'
+    # render json: {name: 'zch', age: 18}
+    render 'first/hello'
   end
 end
