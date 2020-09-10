c4f0cd0feaf5a7ff6132717880f82a536ddf0eca
zhangchao <529743595@qq.com>
2020-09-10 21:04:12 +0800

    Add HTML Layout

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -2,6 +2,10 @@ class FirstController < ApplicationController
   def hello
     # render plain: 'hello rails'
     # render json: {name: 'zch', age: 18}
+    @xxx = 'Controller 里面的 xxx 变量'
     render 'first/hello'
+  end
+  def hi
+    render 'first/hi'
   end
 end
