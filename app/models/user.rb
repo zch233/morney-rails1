b81ac5a2814fa5903640141d5cdb40a1013db7d0
zhangchao <529743595@qq.com>
2020-09-20 21:56:21 +0800

    Add Email Repeat Validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email
+  validates_uniqueness_of :email
   validates_presence_of :password_confirmation, on: [:create]
 
   validates_format_of :email, with: /.+@.+/, if: :email
