270990cff16594a65e89ed04a671768797a2c67c
zhangchao <529743595@qq.com>
2020-09-26 15:29:30 +0800

    Add Customer Validate for Check Email Present

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,7 +3,13 @@ class Session
   attr_accessor :email, :password
 
   validates_presence_of :email, :password
+  validate :check_email_present, if: :email
   validates_format_of :email, with: /.+@.+/, if: :email
   validates_length_of :password, minimum: 6, if: :password
 
+  def check_email_present
+    if (User.find_by_email email).nil?
+      errors.add :email, :email_not_present
+    end
+  end
 end
