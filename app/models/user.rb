a4a7422b8193520e0491ff465a818a2c383875a2
zhangchao <529743595@qq.com>
2020-09-26 16:04:04 +0800

    Fix Email Send Later

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
 
   after_create :send_welcome_email
   def send_welcome_email
-    UserMailer.welcome_email(self).deliver_now
+    UserMailer.welcome_email(self).deliver_later
   end
 end
