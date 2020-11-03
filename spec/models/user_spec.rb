1af0e7774427bc928377ca2d162674148d26a8b5
zhangchao <529743595@qq.com>
2020-11-03 21:54:13 +0800

    test: add user welcome email send test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe User, type: :model do
     user = User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
     expect(user.errors.details[:email][0][:error]).to eq(:taken)
   end
+  it '创建以后会发送邮件给用户' do
+    mailer = spy('Mailer')
+    allow(UserMailer).to receive(:welcome_email).and_return(mailer)
+    User.create! email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(UserMailer).to have_received(:welcome_email)
+    expect(mailer).to have_received(:deliver_later)
+  end
 end
