1c546c7a56d115de35995f3cc292b73d06f34ebe
zhangchao <529743595@qq.com>
2020-11-19 20:43:23 +0800

    test: add a controller test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
+  it 'should create user' do
+    post '/users', params: {email: ''}
+    expect(response.status).to eq 422
+    body = JSON.parse(response.body)
+    expect(body['errors']['email'][0]).to eq '邮箱不能为空'
+    expect(body['errors']['email'].length).to eq 1
+    expect(body['errors']['password'][0]).to eq '密码不能为空'
+    expect(body['errors']['password'].length).to eq 1
+    expect(body['errors']['password_confirmation'][0]).to eq '确认密码不能为空'
+    expect(body['errors']['password_confirmation'].length).to eq 1
+  end
 end
