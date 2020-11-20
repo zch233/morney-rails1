3b3d9d48ea2b3da4c9ecadd2f36f0e100de5868c
zhangchao <529743595@qq.com>
2020-11-20 15:22:30 +0800

    test: test /current_user_info

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe "Users", type: :request do
     get '/current_user_info'
     expect(response).to have_http_status 401
   end
+  it '登录以后可以得到个人信息' do
+    user = post '/users', params: { email: '1@qq.com', password: '123456', password_confirmation: '123456' }
+    session = post '/sessions', params: { email: '1@qq.com', password: '123456' }
+    p user
+    p session
+    get '/current_user_info'
+    expect(user.id).to eq session.id
+  end
 end
