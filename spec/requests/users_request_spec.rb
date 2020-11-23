50f1de472c2456dfbac12468a9b7f725a66680dc
zhangchao <529743595@qq.com>
2020-11-23 16:55:55 +0800

    test: add user test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -12,24 +12,12 @@ RSpec.describe "Users", type: :request do
     expect(body['errors']['password_confirmation'][0]).to eq '确认密码不能为空'
     expect(body['errors']['password_confirmation'].length).to eq 1
   end
-  it '创建账户-只填邮箱' do
-    post '/users', params: {email: '1@qq.com'}
-    expect(response.status).to eq 422
+  it '创建账户-参数齐全' do
+    post '/users', params: {email: '1@qq.com', password: '123456', password_confirmation: '123456'}
+    expect(response).to have_http_status :ok
     body = JSON.parse(response.body)
-    expect(body['errors']['email']).to eq nil
-    expect(body['errors']['password'][0]).to eq '密码不能为空'
-    expect(body['errors']['password'].length).to eq 1
-    expect(body['errors']['password_confirmation'][0]).to eq '确认密码不能为空'
-    expect(body['errors']['password_confirmation'].length).to eq 1
-  end
-  it '创建账户-只填邮箱，密码' do
-    post '/users', params: {email: '1@qq.com', password: '123456'}
-    expect(response.status).to eq 422
-    body = JSON.parse(response.body)
-    expect(body['errors']['email']).to eq nil
-    expect(body['errors']['password']).to eq nil
-    expect(body['errors']['password_confirmation'][0]).to eq '确认密码不能为空'
-    expect(body['errors']['password_confirmation'].length).to eq 1
+    expect(body['errors']).to eq nil
+    expect(body['resource']['id']).to be_a Numeric
   end
   it '未登录无法得到个人信息' do
     get '/current_user_info'
