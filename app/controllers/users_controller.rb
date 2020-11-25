451027a4eb4daf2bfe6f3133f87f4e5d3ace25eb
zhangchao <529743595@qq.com>
2020-11-25 12:05:06 +0800

    feat: add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
   def current_user_info
     render_resource current_user
   end
-
+  def index
+    render_resource User.all
+  end
+  def destroy
+    render_resource User.destroy params[:id]
+  end
   def create_params
     params.permit(:email, :password, :password_confirmation)
   end
