f98f36c95fb2620bf8449da2fbc8242b0bcd62d6
zhangchao <529743595@qq.com>
2020-12-02 19:31:41 +0800

    feat: add record modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_081348) do
+ActiveRecord::Schema.define(version: 2020_12_02_112136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "records", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.integer "category", limit: 2, null: false
+    t.string "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
