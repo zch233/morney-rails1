20d5743c907e808e0abacbaa8d2fb3cb46b02ab9
zhangchao <529743595@qq.com>
2020-12-02 20:31:56 +0800

    test: add record require category

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,4 @@
 class Record < ApplicationRecord
+  validates_presence_of :amount
+  validates_presence_of :category
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Record, type: :model do
     record = Record.create category: 'outgoings', notes: '测试的备注'
     expect(record.errors.details[:amount][0][:error]).to eq(:blank)
   end
+  it '创建时必须要填类型' do
+    record = Record.create amount: 10000, notes: '测试的备注'
+    expect(record.errors.details[:category][0][:error]).to eq(:blank)
+  end
 end
