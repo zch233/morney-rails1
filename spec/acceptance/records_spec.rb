2f56cdf82689a8865804151c362e7797c8f6e28b
zhangchao <529743595@qq.com>
2021-01-07 20:51:06 +0800

    test: add sign_in to helper

--- spec/acceptance/records_spec.rb
+++ spec/acceptance/records_spec.rb
@@ -7,6 +7,7 @@ resource "Records" do
     parameter :category, '类型 outgoings | income', type: :string, require: true
     parameter :notes, '备注', type: :string
     example "创建一条记录" do
+      sign_in
       do_request(amount: 10000, category: 'outgoings', notes: '聚会')
       expect(status).to eq 200
     end
